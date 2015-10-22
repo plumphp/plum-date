30e7c55e18d6577eaea4c72c9731b1e813479b55
Florian Eckerstorfer <florian@eckerstorfer.co>
2015-10-22 17:52:21 +0200

    Add DateTimeToStringConverter
    Fixes #2

---
+++ src/DateTimeToStringConverter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Plum\PlumDate;
+
+use DateTime;
+use InvalidArgumentException;
+use Plum\Plum\Converter\ConverterInterface;
+
+/**
+ * DateTimeToStringConverter
+ *
+ * @package   Plum\PlumDate
+ * @author    Florian Eckerstorfer
+ * @copyright 2015 Florian Eckerstorfer
+ */
+class DateTimeToStringConverter implements ConverterInterface
+{
+    /**
+     * @var string
+     */
+    protected $format;
+
+    /**
+     * @param string $format
+     *
+     * @codeCoverageIgnore
+     */
+    public function __construct($format)
+    {
+        $this->format = $format;
+    }
+
+    /**
+     * @param mixed $item
+     *
+     * @return mixed
+     */
+    public function convert($item)
+    {
+        if (!$item instanceof DateTime) {
+            throw new InvalidArgumentException('The item given to Plum\PlumDate\DateTimeToStringConverter must be '.
+                                                'an instance of DateTime.');
+        }
+
+        return $item->format($this->format);
+    }
+}
